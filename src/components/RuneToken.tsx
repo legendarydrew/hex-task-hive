0f7c4ae371cc8a1882fcb1be2055838c06cd7833
Drew Maughan <drew.maughan@toptal.com>
2025-04-05 15:50:25 +0100

    feat: creating a RuneToken component with a hexagonal appearance.

---
+++ src/components/RuneToken.tsx
@@ -0,0 +1,16 @@
+/**
+ * A component representing a task as a "rune token".
+ * The colour of the token (background or text?) will be determined by the task list's palette.
+ */
+
+import './RuneToken.scss';
+
+function RuneToken(props) {
+  return (
+    <button type="button" className="rune-token">
+      <span className="rune-token-number">{props.taskId}</span>
+    </button>
+  );
+}
+
+export default RuneToken;
