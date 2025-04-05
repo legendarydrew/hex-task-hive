f7ef86ae87b6fc88326646eb6e764be22512355c
Drew Maughan <drew.maughan@toptal.com>
2025-04-05 16:43:19 +0100

    feat: added buttons for each task, displaying in two columns.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,6 @@
 import { useApp } from "@/context/AppContext";
 import { Task } from "@/types";
+import { Check, Trash, Edit } from 'lucide-react';
 import "./TaskList.scss";
 
 /**
@@ -15,16 +16,22 @@ export default function TaskList() {
     : [];
 
   return (
-    // Using a table for now, but we might want to use a two-column flex-layout.
     <ol className="task-list">
       {activeTasks.map((task, index) => (
         <li className="task-list-item" key={index}>
           <b className="task-list-item-number">{index}</b>
           <span className="task-list-item-text">{task.description}</span>
           <menu className="task-list-item-actions">
-            <button type="button" title="Change description">Edit</button>
-            <button type="button" title="Mark as complete">Completed</button>
-            <button type="button" title="Remove">Remove</button>
+            {/* TODO see https://v1.tailwindcss.com/components/buttons for implementing button styles. */}
+            <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold lh-1 py-1 px-2 rounded" title="Change description">
+                <Edit className="h-3 w-3" />
+            </button>
+            <button type="button" className="bg-green-500 hover:bg-green-700 text-white font-bold lh-1 py-1 px-2 rounded" title="Mark as complete">
+                <Check className="h-3 w-3" />
+            </button>
+            <button type="button" className="bg-red-500 hover:bg-red-700 text-white font-bold lh-1 py-1 px-2 rounded" title="Remove">
+                <Trash className="h-3 w-3" />
+            </button>
           </menu>
         </li>
       ))}
