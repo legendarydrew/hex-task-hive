5f6d0f31c1bf0c5a5e00e95f1e2951693255bcea
Drew Maughan <drew.maughan@toptal.com>
2025-04-07 14:16:21 +0100

    feat: ability to update existing tasks inside TaskList.
    A little Tailwind.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -2,6 +2,8 @@ import { useApp } from "@/context/AppContext";
 import { Task } from "@/types";
 import { Check, Undo, Trash, Edit } from "lucide-react";
 import "./TaskList.scss";
+import TaskListUpdate from "./TaskListUpdate";
+import { useState } from "react";
 /**
  * A component simply displaying the list of tasks in the currently selected list.
  * From here we should be able to update or remove the tasks.
@@ -10,13 +12,19 @@ import "./TaskList.scss";
 export default function TaskList(props) {
   const { state, deleteTask, toggleTaskCompletion } = useApp();
 
+  const [taskForEdit, setTaskForEdit] = useState<Task>();
+
   const activeTasks: Task[] = state.activeListId
     ? state.tasks.filter((task) => task.listId === state.activeListId)
     : [];
 
   const beginTaskUpdateHandler = (task: Task) => {
-    console.log("beginTaskUpdateHandler", task);
+    setTaskForEdit(task);
   };
+
+  const closeUpdateHandler = (): void => {
+    setTaskForEdit(undefined); 
+  }
 
   const removeTaskHandler = (task: Task) => {
     // Purely for convenience, we won't bother confirming the removal of the task.
@@ -30,7 +38,11 @@ export default function TaskList(props) {
   return (
     <ol className="task-list">
       {activeTasks.map((task: Task, index) => (
-        <li className="task-list-item" key={index}>
+        <li className="task-list-item items-center" key={index}>
+          { taskForEdit === task ? (
+            <TaskListUpdate task={task} onClose={closeUpdateHandler} />
+          ) : (
+            <>
           <b className="task-list-item-number">{index}</b>
           <span className="task-list-item-text">{task.description}</span>
           <menu className="task-list-item-actions">
@@ -38,7 +50,7 @@ export default function TaskList(props) {
             <button
               type="button"
               onClick={() => beginTaskUpdateHandler(task)}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold lh-1 py-1 px-2 rounded"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold h-6 lh-1 py-1 px-2 rounded"
               title="Change description"
             >
               {/* TODO ability to change the task description and category. */}
@@ -48,7 +60,7 @@ export default function TaskList(props) {
               <button
                 type="button"
                 onClick={() => toggleCompleteHandler(task)}
-                className="bg-orange-500 hover:bg-orange-700 text-white font-bold lh-1 py-1 px-2 rounded"
+                className="bg-orange-500 hover:bg-orange-700 text-white font-bold h-6 lh-1 py-1 px-2 rounded"
                 title="Unmark as complete"
               >
                 <Undo className="h-3 w-3" />
@@ -57,7 +69,7 @@ export default function TaskList(props) {
               <button
                 type="button"
                 onClick={() => toggleCompleteHandler(task)}
-                className="bg-green-500 hover:bg-green-700 text-white font-bold lh-1 py-1 px-2 rounded"
+                className="bg-green-500 hover:bg-green-700 text-white font-bold h-6 lh-1 py-1 px-2 rounded"
                 title="Mark as complete"
               >
                 <Check className="h-3 w-3" />
@@ -72,6 +84,7 @@ export default function TaskList(props) {
               <Trash className="h-3 w-3" />
             </button>
           </menu>
+          </>)}
         </li>
       ))}
     </ol>
---
+++ src/components/TaskListUpdate.tsx
@@ -0,0 +1,113 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { FormEvent, useEffect, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useApp } from "@/context/AppContext";
+import { Check, X } from "lucide-react";
+import { Task } from "@/types";
+/**
+ * This component is used for updating a Task within the Task List.
+ */
+
+export default function TaskListUpdate(props: {
+  task: Task;
+  onClose: Function;
+}) {
+  const inputField = useRef(null);
+
+  const { state, updateTask, getListCategories } = useApp();
+  const [category, setCategory] = useState("");
+  const [description, setDescription] = useState("");
+
+  const categories = state.activeListId
+    ? getListCategories(state.activeListId)
+    : [];
+
+  useEffect(() => {
+    if (props.task) {
+      setDescription(props.task.description);
+      setCategory(props.task.category);
+    } else {
+      setDescription("");
+      setCategory("");
+    }
+    inputField.current.focus();
+  }, [props.task]);
+
+  function changeDescriptionHandler(event) {
+    setDescription(event.target.value);
+  }
+
+  function updateTaskHandler(e: FormEvent) {
+    e.preventDefault();
+    updateTask(props.task.id, { category, description });
+    props.onClose();
+  }
+
+  function cancelHandler(e: FormEvent) {
+    e.preventDefault();
+    props.onClose();
+  }
+
+  return (
+    <form
+      onSubmit={updateTaskHandler}
+      onReset={cancelHandler}
+      className="flex gap-1 w-full items-center"
+    >
+      <div className="w-1/4">
+        <Select value={category} onValueChange={setCategory}>
+          <SelectTrigger className="p-1 h-6" id="category">
+            <SelectValue placeholder="Category" />
+          </SelectTrigger>
+          <SelectContent>
+            {categories.map((cat) => (
+              <SelectItem key={cat} value={cat}>
+                {cat}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+
+      <div className="flex-grow">
+        <Input
+          className="p-1 h-6"
+          ref={inputField}
+          type="text"
+          value={description}
+          placeholder="Enter a task description..."
+          onChange={changeDescriptionHandler}
+          required
+        />
+      </div>
+
+      <div className="flex gap-1">
+        <Button
+          className="h-6 w-6"
+          type="submit"
+          variant="default"
+          size="sm"
+          title="Confirm edit"
+        >
+          <Check className="h-2 w-2" />
+        </Button>
+        <Button
+          className="h-6 w-6"
+          type="reset"
+          variant="destructive"
+          size="sm"
+          title="Cancel edit"
+        >
+          <X className="h-2 w-2" />
+        </Button>
+      </div>
+    </form>
+  );
+}
