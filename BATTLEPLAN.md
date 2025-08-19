ce8c60cefa725338189b99452aa0f642bc1647ac
Drew Maughan <drew.maughan@toptal.com>
2025-08-19 16:35:48 +0100

    chore: updating BATTLEPLAN.md.

--- BATTLEPLAN.md
+++ BATTLEPLAN.md
@@ -6,14 +6,15 @@
 - [ ] Input and dropdown fields.
 - [ ] Buttons.
 - [x] Rune tokens.
-- [ ] A background image for the tokens area.
+- [x] A background image for the tokens area.
 
 ## Features
 - [ ] Display a banner ad?
 - [x] Ability to add tasks in bulk.
-- [ ] Toggle between single and bulk task creation.
+- [x] Toggle between single and bulk task creation.
 - [ ] Ability to set categories for the current list.
 - [x] Fix the algorithm for displaying tokens in a hexagonal grid.
+- [ ] Confetti on completion!
 
 ## Miscellaneous
 - [ ] An about page/dialogue.
--- src/components/TaskListItem.tsx
+++ src/components/TaskListItem.tsx
@@ -4,67 +4,71 @@ import { Check, Edit, Trash, Undo } from "lucide-react";
 import { Button } from "./ui/button";
 
 interface TaskListItemProps {
-    taskNumber: number;
-    task: Task;
-    onEdit: (task: Task) => void
+  taskNumber: number;
+  task: Task;
+  onEdit: (task: Task) => void;
 }
 
 export const TaskListItem: React.FC<TaskListItemProps> = ({
-                                                              taskNumber,
-                                                              task,
-                                                              onEdit
-                                                          }) => {
-    const { deleteTask, toggleTaskCompletion } = useApp();
+  taskNumber,
+  task,
+  onEdit,
+}) => {
+  const { deleteTask, toggleTaskCompletion } = useApp();
 
-    const beginTaskUpdateHandler = (task: Task) => {
-        onEdit(task);
-    };
+  const beginTaskUpdateHandler = (task: Task) => {
+    onEdit(task);
+  };
 
-    const removeTaskHandler = (task: Task) => {
-        // Purely for convenience, we won't bother confirming the removal of the task.
-        deleteTask(task.id);
-    };
+  const removeTaskHandler = (task: Task) => {
+    // Purely for convenience, we won't bother confirming the removal of the task.
+    deleteTask(task.id);
+  };
 
-    const toggleCompleteHandler = (task: Task) => {
-        toggleTaskCompletion(task.id);
-    };
+  const toggleCompleteHandler = (task: Task) => {
+    toggleTaskCompletion(task.id);
+  };
 
-    return (
-        <>
-            <span className="heading-text text-right w-8">{taskNumber}</span>
-            <span className="flex-grow">{task.description}</span>
-            <menu className="flex gap-0.5 justify-end">
-                <Button
-                    type="button"
-                    className="rounded-none"
-                    variant="secondary"
-                    size="icon"
-                    onClick={() => beginTaskUpdateHandler(task)}
-                    title="Change description"
-                >
-                    <Edit className="h-3 w-3"/>
-                </Button>
-                <Button
-                    type="button"
-                    className="rounded-none"
-                    variant={task.completedAt ? 'outline' : 'confirm'}
-                    size="icon"
-                    onClick={() => toggleCompleteHandler(task)}
-                    title={task.completedAt ? 'Unmark as complete' : 'Mark as complete'}
-                >
-                    {task.completedAt ? <Undo className="h-3 w-3"/> : <Check className="h-3 w-3"/>}
-                </Button>
-                <Button
-                    type="button"
-                    className="rounded-r rounded-l-none"
-                    variant="destructive"
-                    size="icon"
-                    onClick={() => removeTaskHandler(task)}
-                    title="Remove"
-                >
-                    <Trash className="h-3 w-3"/>
-                </Button>
-            </menu>
-        </>
-    );
+  return (
+    <>
+      <span className="heading-text text-right w-8">{taskNumber}</span>
+      <span className="flex-grow">{task.description}</span>
+      <menu className="flex gap-0.5 justify-end">
+        <Button
+          type="button"
+          className="rounded-none"
+          variant="secondary"
+          size="icon"
+          onClick={() => beginTaskUpdateHandler(task)}
+          title="Change description"
+        >
+          <Edit className="h-3 w-3" />
+        </Button>
+        <Button
+          type="button"
+          className="rounded-none"
+          variant={task.completedAt ? "outline" : "confirm"}
+          size="icon"
+          onClick={() => toggleCompleteHandler(task)}
+          title={task.completedAt ? "Unmark as complete" : "Mark as complete"}
+        >
+          {task.completedAt ? (
+            <Undo className="h-3 w-3" />
+          ) : (
+            <Check className="h-3 w-3" />
+          )}
+        </Button>
+        <Button
+          type="button"
+          className="rounded-r rounded-l-none"
+          variant="destructive"
+          size="icon"
+          onClick={() => removeTaskHandler(task)}
+          title="Remove"
+        >
+          <Trash className="h-3 w-3" />
+        </Button>
+      </menu>
+    </>
+  );
 };
